c60b12a4e5730c5c68c63dfcf983345f99f4366f
Durgesh Kuchi <durgesh@joinvulse.com>
2025-03-01 20:35:54 -0600

    added github pages

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <Router>
+    <Router basename="/curly-waddle">
       <Routes>
         <Route path="/" element={<Login />} />
         <Route
