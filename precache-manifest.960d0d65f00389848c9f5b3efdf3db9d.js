1f153ce3eaaaef9de2838b009a5cb11b75467328
Durgesh Kuchi <durgesh@joinvulse.com>
2025-03-01 20:23:53 -0600

    Updates

---
+++ precache-manifest.960d0d65f00389848c9f5b3efdf3db9d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d45dd72eb3686fa84fed90fac8c7706",
+    "url": "/curly-waddle/index.html"
+  },
+  {
+    "revision": "1f6d422e3b5a7dbea6d2",
+    "url": "/curly-waddle/static/css/main.d3232ac4.chunk.css"
+  },
+  {
+    "revision": "fe3e06514d88691d0dfd",
+    "url": "/curly-waddle/static/js/2.8d765f24.chunk.js"
+  },
+  {
+    "revision": "1aef9e0d481a6908c30509eb88500877",
+    "url": "/curly-waddle/static/js/2.8d765f24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f6d422e3b5a7dbea6d2",
+    "url": "/curly-waddle/static/js/main.7a4905fc.chunk.js"
+  },
+  {
+    "revision": "bc18427cff1f17bd5b8a",
+    "url": "/curly-waddle/static/js/runtime-main.88588fc8.js"
+  },
+  {
+    "revision": "b50cde19e9e41d7b3741f7308cdc0c27",
+    "url": "/curly-waddle/static/media/SmartBankerLogo.b50cde19.png"
+  },
+  {
+    "revision": "17f5c2121471dbf848be60796a3dfdde",
+    "url": "/curly-waddle/static/media/logo.17f5c212.png"
+  }
+]);
